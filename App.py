fc5340774077718b45abae66b3e9380bd1f80c9b
supriyadc2006-commits <supriyadc2006@gmail.com>
2026-04-15 10:33:08 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_congig("PragyanAI Content Generator",layout="wide")
+st.title("PragyanAI - Content Generator")
+st.image("PragyanAI_Transperent.jpg")
